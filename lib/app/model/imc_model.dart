0e6e348378e6de1ed30c57c2b6cb8ba3e9350abe
Gabriel FV <gabrielvasco906@gmail.com>
2024-10-14 17:32:15 -0300

    Refactor ImcModel and ImcViewModel classes

--- lib/app/model/imc_model.dart
+++ lib/app/model/imc_model.dart
@@ -1,9 +1,16 @@
 class ImcModel {
-  final double weight;
-  final double height;
+  double weight;
+  double height;
+  double imc;
 
   ImcModel({
     required this.weight,
     required this.height,
+    this.imc = 0.0,
   });
+
+  double calculateImc() {
+    imc = weight / (height * height);
+    return imc;
+  }
 }
--- lib/app/view_models/imc_view_model.dart
+++ lib/app/view_models/imc_view_model.dart
@@ -1,12 +1,23 @@
 import 'package:calcular_imc/app/model/imc_model.dart';
+import 'package:flutter/material.dart';
 
-class ImcViewModel extends ImcModel {
-  ImcViewModel({
-    required super.weight,
-    required super.height,
-  });
+class ImcViewModel extends ChangeNotifier {
+  final ImcModel _imcModel = ImcModel(weight: 0, height: 0);
 
-  double calculateImc() {
-    return weight / (height * height);
+  double get imc => _imcModel.imc;
+
+  void setHeight(double height) {
+    _imcModel.height = height;
+    notifyListeners();
+  }
+
+  void setWeight(double weight) {
+    _imcModel.weight = weight;
+    notifyListeners();
+  }
+
+  void calculateImc() {
+    _imcModel.calculateImc();
+    notifyListeners();
   }
 }
